7ef576a872e122d62a96e41663949640e70d0ab1
adflashik <adflashik@gmail.com>
2026-06-28 15:15:43 -1000

    Add a script for quick local backend start

---
+++ backend/scripts/quick_local_start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Building and spinning up dockerized environment..."
+# Alsways start the containers in detached mode and build the images
+docker compose up -d --build
+
+echo "Updating the database..."
+docker compose exec -T web alembic upgrade head
+
+echo "Seeding temporary test data..."
+docker compose exec -T web python seed.py
+
+echo "Executing backend tests..."
+source backend/qa/scripts/run_tests.sh
+echo "NOTE: Docker containers are left running for manual testing."
+echo "    - Local Server: http://localhost:5000"
+echo "    - API Docs:     http://localhost:5000/docs"
+echo "    - To stop the containers, run: docker compose down"
