371778663499da3b87bd6770db6267030ff5a036
Jesse Abdul <jdcabdul@hawaii.edu>
2026-07-11 16:27:25 -1000

    bug fix in the directory path variable

--- frontend/scripts/quick_local_start.sh
+++ frontend/scripts/quick_local_start.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 # change to the frontend directory
-cd parent_path/..
+cd "${parent_path}/.."
 
 echo "installing required npm packages for the frontend"
 # install required npm packages
@@ -12,3 +12,4 @@ npm install
 echo "running npm frontend server"
 # run NPM server
 npm run dev
+4
