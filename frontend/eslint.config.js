bf16ca655581ac8e262959a84b0269dbf2aa9986
Jesse Abdul <jdcabdul@hawaii.edu>
2026-07-27 15:49:03 -1000

    Added sonarjs plugin for ESLint and configured for warnings for cognitive complexity > 15, and identify any duplicated branches, identical functions, or duplicate strings configured ESLint to check for complexity (> 12) and max depth (> 4) for the frontend code

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import noUnsanitized from "eslint-plugin-no-unsanitized";
+import sonarjs from "eslint-plugin-sonarjs";
 
 export default defineConfig([
   globalIgnores([
@@ -16,10 +17,17 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     plugins: {
       "no-unsanitized": noUnsanitized,
+      sonarjs,
     },
     rules: {
       "no-unsanitized/method": "error",
       "no-unsanitized/property": "error",
+      complexity: ["warn", { max: 12 }],
+      "max-depth": ["warn", 4],
+      "sonarjs/cognitive-complexity": ["warn", 15],
+      "sonarjs/no-duplicated-branches": "warn",
+      "sonarjs/no-identical-functions": "warn",
+      "sonarjs/no-duplicate-string": "warn",
     },
     extends: [
       js.configs.recommended,
